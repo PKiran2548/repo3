c167d8545ec2881d75d335604a531ceff5e34f9e
PKiran2548 <kiranpaithane@gmail.com>
2023-01-31 13:56:54 +0530

    Changes made by kiran

---
+++ src/main/java/com/RenuIT/SpringGitDemo3Application.java
@@ -0,0 +1,13 @@
+package com.RenuIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitDemo3Application.class, args);
+	}
+
+}
